d7182f864e39b03f9454b740da1f8119020bf9fb
Pablo Felgueres <pablofelgueres@gmail.com>
2025-08-12 23:29:26 -0700

    Update nvim_init.lua

--- nvim_init.lua
+++ nvim_init.lua
@@ -1,23 +1,23 @@
--- ~/.config/nvim/init.lua
-vim.opt.number = true
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
-vim.opt.smartindent = true
-vim.opt.wrap = false
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.hlsearch = false
-vim.opt.termguicolors = true
-vim.o.clipboard = 'unnamedplus'
+-- brew install lua-language-server pyright typescript-language-server
 
--- lazy.vim is the package manager
+vim.o.number = true
+vim.o.relativenumber = false
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.opt.virtualedit = "block"
+vim.opt.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
+vim.keymap.set("n", "<Space>", "<Nop>")
+vim.g.mapleader = " "
+vim.keymap.set({'n', 'v'}, '<leader>y', '"+y', { desc = 'Copy to clipboard' })
+vim.keymap.set({'n', 'v'}, '<leader>p', '"+p', { desc = 'Paste from clipboard' })
+
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
+    "git", "clone", "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
     "--branch=stable", -- latest stable release
     lazypath,
@@ -25,54 +25,116 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Load lazy.nvim
 require("lazy").setup({
-  {
-    "melbaldove/llm.nvim",
-    dependencies = { "nvim-neotest/nvim-nio" }
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  { "nvim-lualine/lualine.nvim" },
+  { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+  { "folke/tokyonight.nvim", priority = 1000, config = function()
+   require("tokyonight").setup({
+     style = "night", -- storm, moon, night, day
+     transparent = false,
+     terminal_colors = true,
+     styles = {
+       comments = { italic = true },
+       keywords = { italic = true },
+       functions = {},
+       variables = {},
+     },
+   })
+   vim.cmd("colorscheme tokyonight-night") end, lazy = false,},
+     {
+    "nvim-lualine/lualine.nvim",
+    config = function()
+      require("lualine").setup()
+    end,
   },
-  {
-    "keith/swift.vim"
+    {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { "lua", "python", "javascript", "html", "css" },
+        highlight = { enable = true },
+      })
+    end,
   },
-  {
-    'numToStr/Comment.nvim',
-    lazy=false
-  }
+    {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+      vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+    end,
+  },
+    -- --- completion & snippets ----------------------------------------------
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+          "L3MON4D3/LuaSnip",
+          "saadparwaiz1/cmp_luasnip",
+          "hrsh7th/cmp-nvim-lsp",
+          "hrsh7th/cmp-buffer",
+          "hrsh7th/cmp-path",
+        },
+        config = function()
+          local cmp = require("cmp")
+          local luasnip = require("luasnip")
+    
+          require("luasnip.loaders.from_vscode").lazy_load() -- optional; needs friendly-snippets (add below) if you want many snippets
+    
+          cmp.setup({
+            snippet = {
+              expand = function(args) luasnip.lsp_expand(args.body) end,
+            },
+            mapping = cmp.mapping.preset.insert({
+              ["<CR>"] = cmp.mapping.confirm({ select = true }),
+              ["<Tab>"] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+                  cmp.select_next_item()
+                elseif luasnip.expand_or_jumpable() then
+                  luasnip.expand_or_jump()
+                else
+                  fallback()
+                end
+              end, { "i", "s" }),
+              ["<S-Tab>"] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+                  cmp.select_prev_item()
+                elseif luasnip.jumpable(-1) then
+                  luasnip.jump(-1)
+                else
+                  fallback()
+                end
+              end, { "i", "s" }),
+            }),
+            sources = {
+              { name = "nvim_lsp" },
+              { name = "luasnip" },
+              { name = "path" },
+              { name = "buffer" },
+            },
+          })
+        end,
+      },
+        {
+    "neovim/nvim-lspconfig",
+    dependencies = { "hrsh7th/cmp-nvim-lsp" },
+    config = function()
+      local lsp = require("lspconfig")
+      local caps = require("cmp_nvim_lsp").default_capabilities()
+
+      -- example servers (install them separately)
+      lsp.lua_ls.setup({
+        capabilities = caps,
+        settings = { Lua = { diagnostics = { globals = { "vim" } } } },
+      })
+      lsp.pyright.setup({ capabilities = caps })
+      -- add more: lsp.tsserver.setup({ capabilities = caps })  -- if you use TypeScript/JS
+    end,
+  },
+
 })
 
-require('Comment').setup()
-
--- Set leader key to space
-vim.g.mapleader = " "
-
--- Basic keymaps
-vim.keymap.set('n', '<leader>w', ':w<CR>')
-vim.keymap.set('n', '<leader>q', ':q<CR>')
-
--- Tab navigation keymaps
-vim.api.nvim_set_keymap('n', 'tn', ':tabnext<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', 'tp', ':tabprev<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', 'to', ':tabnew<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>tc', ':tabclose<CR>', { noremap = true, silent = true })
-
--- yank to system clipboard
-vim.api.nvim_set_keymap('n', '<leader>y', '"+y', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('v', '<leader>y', '"+y', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>Y', '"+Y', { noremap = true, silent = true })
-
-
-
--- Keybindings for llm.nvim functions
-vim.keymap.set("n", "<leader>m", function() require("llm").create_llm_md() end)
-
--- Keybinds for prompting with openai
-vim.keymap.set("n", "<leader>g,", function() require("llm").prompt({ replace = false, service = "openai" }) end)
-vim.keymap.set("v", "<leader>g,", function() require("llm").prompt({ replace = false, service = "openai" }) end)
-vim.keymap.set("v", "<leader>g.", function() require("llm").prompt({ replace = true, service = "openai" }) end)
-
--- Config to autodetect swift files
-vim.cmd([[autocmd BufRead,BufNewFile *.swift set filetype=swift]])
-
-vim.cmd([[colorscheme desert]])
-vim.cmd('syntax enable')
-vim.cmd('syntax on')
